b3b12f779de42a967e9092042d5104e344837205
Kamenski19S <s17kemenski@gmail.com>
2026-09-19 01:38:26 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1345,6 +1345,7 @@ function Midgard3D({ h, on, eventDone }: { h: HeroDef; on: (id: string) => void;
             mats.forEach((m:any) => {
               if ('roughness' in m) m.roughness = 0.94;
               if ('metalness' in m) m.metalness = 0.0;
+              if (m.color?.isColor) m.color.multiplyScalar(0.98);
             });
           }
         });
@@ -1400,7 +1401,7 @@ function Midgard3D({ h, on, eventDone }: { h: HeroDef; on: (id: string) => void;
             mats.forEach((m:any) => {
               if ('roughness' in m) m.roughness = 0.94;
               if ('metalness' in m) m.metalness = 0.0;
-              if (m.color?.isColor) m.color.multiplyScalar(0.75);
+              if (m.color?.isColor) m.color.multiplyScalar(0.8625);
             });
           }
         });
